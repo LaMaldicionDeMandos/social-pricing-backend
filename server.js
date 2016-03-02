ddec81a18ce3c35fde552a3005ce139347786081
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-01 23:20:06 -0300

    instalo elasticsearch y implemento una query de ejemplo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var es = require('elasticsearch');
 var xnconfig = require('nodejsconfig');
 var data = fs.readFileSync(__dirname+'/config/config.properties', 'UTF8');
 config = xnconfig.parse(process.env.NODE_ENV || 'development', data);
@@ -6,9 +7,32 @@ console.log("Env: " + config.env);
 var express = require('express');
 var app = express();
 
+var esClient = new es.Client({host: config.elastic_host, log:'info'});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
+  /* Ejemplo de query en elastic search 
+  esClient.search({
+    index: 'market',
+    body: {
+      query: {
+        match: {
+          name: {
+            query: 'Carefour Quilmes',
+            fuzziness: "AUTO",
+            operator: 'and'
+          }
+        }
+      }
+    }
+  }).then(function (body) {
+    console.log(JSON.stringify(body));
+    var hits = body.hits.hits;
+  }, function (error) {
+    console.trace(error.message);
+  });
+  */
   res.sendStatus(200);
 });
 
