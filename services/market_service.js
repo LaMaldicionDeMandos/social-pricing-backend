ea2cfce3aa00354b8fcd82db9bc65fbcc4c186ea
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-19 12:05:54 -0300

    creo el market service y el test

---
+++ services/market_service.js
@@ -0,0 +1,3 @@
+/**
+ * Created by boot on 3/19/16.
+ */
---
+++ test/market_service_test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by boot on 3/19/16.
+ */
+describe('Market Service', function() {
+   describe('Searching markets', function() {
+       describe('Search markets and not found results', function() {
+            it('When search by name should return an empty array', function() {
+
+            });
+            it('when search by address should return an empty array', function() {
+
+            });
+            it('when search by geo should return an empty array', function() {
+
+            });
+       });
+       describe('Search markets and return some', function() {
+           it('When search by name should return an array', function() {
+
+           });
+           it('when search by address should return an array', function() {
+
+           });
+           it('when search by geo should return an array', function() {
+
+           });
+       });
+   }) ;
+});
