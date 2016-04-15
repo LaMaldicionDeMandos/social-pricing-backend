90847d550e28649375bf4b84ed71e80b13d710c6
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-04-15 20:12:22 -0300

    Implemento Market service

--- services/market_service.js
+++ services/market_service.js
@@ -1,3 +1,16 @@
 /**
  * Created by boot on 3/19/16.
  */
+function Service(db) {
+    this.searchByName = function(name) {
+        return db.searchMarketByName(name);
+    };
+    this.searchByAddress = function(address, locale) {
+        return db.searchMarketByAddress(address, locale);
+    };
+    this.searchByGeo = function(lat, lon) {
+        return db.searchMarketByGeo(lat, lon);
+    };
+}
+
+module.exports = Service;
--- test/market_service_test.js
+++ test/market_service_test.js
@@ -1,29 +1,154 @@
 /**
  * Created by boot on 3/19/16.
  */
-describe('Market Service', function() {
-   describe('Searching markets', function() {
-       describe('Search markets and not found results', function() {
-            it('When search by name should return an empty array', function() {
+var assert = require('assert');
 
+describe('Market Service', function() {
+    var shouldFail = false;
+    var shouldReturn = [];
+    var search = function(name) {
+        return {
+            then: function(success, error) {
+                if(shouldFail) {
+                    error('error');
+                } else {
+                    success(shouldReturn);
+                }
+            }
+        }
+    };
+    var dbMock = {
+        searchMarketByName: search,
+        searchMarketByAddress: search,
+        searchMarketByGeo: search
+    };
+    var service = new (require('../services/market_service'))(dbMock);
+    describe('Searching markets', function() {
+        describe('Search markets and fail', function() {
+            beforeEach(function() {
+                shouldFail = true;
+            });
+            it('When search by name should fail', function() {
+                return service.searchByName('name').then(
+                    function(markets) {
+                        assert.ok(false);
+                    },
+                    function(err) {
+                        assert.equal(err,'error');
+                    });
+            });
+            it('when search by address should fail', function() {
+                return service.searchByAddress('address', 'locale').then(
+                    function(markets) {
+                        assert.ok(false);
+                    },
+                    function(err) {
+                        assert.equal(err, 'error');
+                    });
+            });
+            it('when search by geo should fail', function() {
+                return service.searchByGeo('-13', '-58').then(
+                    function(markets) {
+                        assert.ok(false);
+                    },
+                    function(err) {
+                        assert.equal(err, 'error');
+                    });
+            });
+        });
+        describe('Search markets and not found results', function() {
+            beforeEach(function() {
+                shouldFail = false;
+                shouldReturn = [];
+            });
+            it('When search by name should return an empty array', function() {
+                return service.searchByName('name').then(
+                    function(markets) {
+                        assert.equal(markets, shouldReturn);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
             });
             it('when search by address should return an empty array', function() {
-
+                return service.searchByAddress('address', 'locale').then(
+                    function(markets) {
+                        assert.equal(markets, shouldReturn);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
             });
             it('when search by geo should return an empty array', function() {
-
+                return service.searchByGeo('13', '13').then(
+                    function(markets) {
+                        assert.equal(markets, shouldReturn);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
             });
-       });
-       describe('Search markets and return some', function() {
-           it('When search by name should return an array', function() {
-
-           });
-           it('when search by address should return an array', function() {
-
-           });
-           it('when search by geo should return an array', function() {
-
-           });
-       });
-   }) ;
+        });
+        describe('Search markets and return some', function() {
+            beforeEach(function() {
+                shouldFail = false;
+                shouldReturn = [{
+                    "id": "a21e01a3-8913-4fc5-a777-3bbd3027396b",
+                    "name": "Crrefour",
+                    "address": "Av. La plata 15555",
+                    "normalizedAddress": "av. la plata 15555",
+                    "locale": "Quilmes Oeste",
+                    "geo": {
+                        "lat": -34.1667,
+                        "lon": -58.1144
+                    }
+                }];
+            });
+            it('When search by name should return an array', function() {
+                return service.searchByName('name').then(
+                    function(markets) {
+                        assert.equal(markets[0].id, 'a21e01a3-8913-4fc5-a777-3bbd3027396b');
+                        assert.equal(markets[0].name, 'Crrefour');
+                        assert.equal(markets[0].address, 'Av. La plata 15555');
+                        assert.equal(markets[0].normalizedAddress, 'av. la plata 15555');
+                        assert.equal(markets[0].locale, 'Quilmes Oeste');
+                        assert.equal(markets[0].geo.lat, -34.1667);
+                        assert.equal(markets[0].geo.lon, -58.1144);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
+            });
+            it('when search by address should return an array', function() {
+                return service.searchByAddress('address', 'locale').then(
+                    function(markets) {
+                        assert.equal(markets[0].id, 'a21e01a3-8913-4fc5-a777-3bbd3027396b');
+                        assert.equal(markets[0].name, 'Crrefour');
+                        assert.equal(markets[0].address, 'Av. La plata 15555');
+                        assert.equal(markets[0].normalizedAddress, 'av. la plata 15555');
+                        assert.equal(markets[0].locale, 'Quilmes Oeste');
+                        assert.equal(markets[0].geo.lat, -34.1667);
+                        assert.equal(markets[0].geo.lon, -58.1144);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
+            });
+            it('when search by geo should return an array', function() {
+                return service.searchByGeo("-34.1667", "-58.1144").then(
+                    function(markets) {
+                        assert.equal(markets[0].id, 'a21e01a3-8913-4fc5-a777-3bbd3027396b');
+                        assert.equal(markets[0].name, 'Crrefour');
+                        assert.equal(markets[0].address, 'Av. La plata 15555');
+                        assert.equal(markets[0].normalizedAddress, 'av. la plata 15555');
+                        assert.equal(markets[0].locale, 'Quilmes Oeste');
+                        assert.equal(markets[0].geo.lat, -34.1667);
+                        assert.equal(markets[0].geo.lon, -58.1144);
+                    },
+                    function() {
+                        assert.ok(false);
+                    });
+            });
+        });
+    }) ;
 });
